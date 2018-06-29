29067971e6379092241e8a816fa11d7e2421f2a9
lugusliu <lugusliu@163.com>
2018-06-29 16:58:20 +0800

    feat:add get put delete methods

--- app.js
+++ app.js
@@ -15,7 +15,20 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// add new work
 app.post('/api/work', workController.addWork);
+
+// get all work
+app.get('/api/work', workController.getWork);
+
+// get work by id
+app.get('/api/work/id/:workId', workController.getWorkById);
+
+// update work by id
+app.put('/api/work/id/:workId', workController.updateWorkById);
+
+// delete work by id
+app.delete('/api/work/id/:workId', workController.deleteWorkById);
 
 app.listen(port, () => {
   console.log(chalk.bgGreen(`Server running at http://${host}:${port}/`));
--- app/work/workController.js
+++ app/work/workController.js
@@ -1,11 +1,9 @@
 const connection = require('../lib/database').connection;
 
 module.exports = {
-  addWork (req, res) {
-    // access the data in the request body
+  addWork(req, res) {
+    // accessing the data in the request body
     const requestData = req.body;
-
-    // console.log(requestData);
 
     const workInfo = {
       userId: requestData.userId,
@@ -20,10 +18,86 @@ module.exports = {
     connection.query("INSERT INTO work SET ?", workInfo, (err, result, fields) => {
       if (err) {
         console.log(err);
-        return res.status(500).json({ code: 1, message: 'error occurred while add work information!!!'});
+        return res.status(500).json({ code: 1, message: 'error occurred while add work information' });
       }
 
-      return res.status(200).json({ code: 0, message: 'work information has been successfully added!!! '});
+      return res.status(200).json({ code: 0, message: 'work information has been successfully added' });
     });
+  },
+
+  getWork(req, res) {
+    connection.query("SELECT * FROM work", (err, result, fields) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ code: 1, message: 'error occurred while get work information from database' });
+      }
+      return res.status(200).json({ code: 0, data: result });
+    });
+  },
+
+  getWorkById(req, res) {
+    // accessing the work id
+    const workId = req.params.workId;
+
+    connection.query("SELECT * FROM work where id = ?", workId, (err, result, fields) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ code: 1, message: 'error occurred while get work information by id from database' });
+      }
+      if (result.length === 0) {
+        return res.status(404).json({ code: 1, message: 'work information by this id not present in database'});
+      }
+      return res.status(200).json({ code: 0, data: result })
+    });
+  },
+
+  updateWorkById(req, res) {
+    // accessing the work id
+    const workId = req.params.workId;
+
+    // accessing the data in the request body
+    const requestData = req.body;
+
+    connection.query("SELECT * from work where id = ?", workId, (err, result, fields) => {
+      if (err) {
+        console.log(err)
+        return res.status(500).json({ code: 1, message: 'error occurred while finding work information by id from database' });
+      }
+      if (result.length === 0) {
+        return res.status(404).json({ code: 1, message: 'work information by this id not present in database'});
+      }
+      connection.query("UPDATE work SET name=?, description=? where id = ?", [requestData.name, requestData.description, workId], (err, result, fields) => {
+        if (err) {
+          console.log(err)
+          return res.status(500).json({ code: 1, message: 'error occurred while updating work information by id from database' });      
+        }
+        res.status(200).json({ code: 0, message: 'work information has been successfully added'});
+      });
+    })
+  },
+
+  deleteWorkById(req, res) {
+    // accessing the work id
+    const workId = req.params.workId;
+
+    // accessing the data in the request body
+    const requestData = req.body;
+
+    connection.query("SELECT * from work where id = ?", workId, (err, result, fields) => {
+      if (err) {
+        console.log(err)
+        return res.status(500).json({ code: 1, message: 'error occurred while finding work information by id from database' });
+      }
+      if (result.length === 0) {
+        return res.status(404).json({ code: 1, message: 'work information by this id not present in database'});
+      }
+      connection.query("DELETE FROM work where id = ?", [workId], (err, result, fields) => {
+        if (err) {
+          console.log(err)
+          return res.status(500).json({ code: 1, message: 'error occurred while deleting work information by id from database' });      
+        }
+        res.status(200).json({ code: 0, message: 'work information has been successfully deleted'});
+      });
+    })
   }
 }
