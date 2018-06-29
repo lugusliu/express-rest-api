18987a08ab5a22ba05facab11e08598c48f970d5
lugusliu <lugusliu@163.com>
2018-06-29 14:00:38 +0800

    chore: add chalk

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const chalk = require('chalk');
 const connection = require('./app/lib/database').connection;
 
 const workController = require('./app/work/workController');
 
 const host = '127.0.0.1';
-const port = 8080;
+const port = 8888;
 
 // create express instance
 const app = express();
 
+// applying middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post('/api/work', workController.addWork);
 
 app.listen(port, () => {
-  console.log(`Server running at http://${host}:${port}/`)
+  console.log(chalk.bgGreen(`Server running at http://${host}:${port}/`));
 })
